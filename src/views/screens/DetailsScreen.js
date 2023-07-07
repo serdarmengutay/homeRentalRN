4cb0ee531d895de48f8e48fc7d96fc37b8f100b2
Serdar Mengütay <serdarmngty03@gmail.com>
2023-07-07 17:23:58 +0300

    home rental app created

---
+++ src/views/screens/DetailsScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SafeAreaView,View, Text } from 'react-native'
+
+const DetailsScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>DetailsScreen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default DetailsScreen
---
+++ src/views/screens/OnBoardScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SafeAreaView,View, Text } from 'react-native'
+
+const OnBoardScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>OnBoardScreen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default OnBoardScreen;
